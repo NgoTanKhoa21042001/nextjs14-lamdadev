28638c70755b4c67c7c8af57d5192b36c0bce4ef
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-03-04 16:03:44 +0700

    Next.js Components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "../components/navbar/Navbar.jsx";
+import Footer from "../components/footer/Footer.jsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,9 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>This is main Layout</h1>
-
+        {" "}
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
