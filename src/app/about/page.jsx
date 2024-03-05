a19ee943215d208f3767720ed2a1fc62de67d8d8
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-03-05 11:28:35 +0700

    Homepage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
-
+import styles from "./about.module.css";
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}></div>
+      <Image src="/about.png" alt="" width={500} height={500} />
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>NgoKhoa</div>
+      <div className={styles.text}>
+        Khoa creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
